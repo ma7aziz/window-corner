3c1a17590725c54d48b3402db781e1a4b72fd540
ma7aziz <45315298+ma7aziz@users.noreply.github.com>
2020-12-18 22:33:58 +0200

    contact page

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('' , views.index ,  name='index')
+    path('', views.index,  name='index'),
+    path('about-us', views.about, name='about'),
+    path('contact', views.contact, name='conatct')
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
-    return render(request , 'core/index.html')
+    return render(request, 'core/index.html')
 
 
-# def about(request):
-#     return render(request , 'about.html')
+def about(request):
+    return render(request, 'core/about.html')
+
+
+def contact(request):
+    return render(request, 'core/contact.html')
