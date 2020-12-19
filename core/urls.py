ffbc7399a24f94bc1873fc9f569e0f0aacc441a7
ma7aziz <45315298+ma7aziz@users.noreply.github.com>
2020-12-19 07:56:45 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index,  name='index'),
     path('about-us', views.about, name='about'),
-    path('contact', views.contact, name='conatct')
+    path('contact', views.contact, name='contact')
 ]
